9bc0e2434d06726c5c5a5d679c2c3a26f8dab4fb
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-08-05 10:06:32 +0530

    feat: add jsonwebtoken for token generation and enhance user model with access/refresh token methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,17 +1,19 @@
-import mongoose, {Schema} from "mongoose";
-import bcrypt from "bcrypt"
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
     {
         avatar: {
-            type:{
-                url:String,
-                localPath: String
+            type: {
+                url: String,
+                localPath: String,
             },
             default: {
-                url:`https://placehold.co/200x200`,
-                localPath: ""
-            }
+                url: `https://placehold.co/200x200`,
+                localPath: "",
+            },
         },
         username: {
             type: String,
@@ -19,7 +21,7 @@ const userSchema = new Schema(
             unique: true,
             lowercase: true,
             trim: true,
-            index: true //for better search
+            index: true, //for better search
         },
         email: {
             type: String,
@@ -35,41 +37,79 @@ const userSchema = new Schema(
         },
         password: {
             type: String,
-            required: [true, "Password is required"]
+            required: [true, "Password is required"],
         },
         isEmailVerified: {
             type: Boolean,
-            default: false
+            default: false,
         },
         refreshToken: {
-            type: String
+            type: String,
         },
         forgotPasswordToken: {
-            type: String
+            type: String,
         },
         forgotPasswordExpiry: {
-            type: Date
+            type: Date,
         },
         emailVerificationToken: {
-            type: String
+            type: String,
         },
         emailVerificationExpiry: {
-            type: Date
-        }
-    }, {
-        timestamps: true
+            type: Date,
+        },
+    },
+    {
+        timestamps: true,
     },
 );
 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
 
-export const User = mongoose.model("User", userSchema)
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+    const unHashedToken = crypto.randomBytes(32).toString("hex");
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex");
+
+    const tokenExpiry = Date.now() + (20 * 60 * 1000); // 20 minutes
+    return { unHashedToken, hashedToken, tokenExpiry };
+};
+export const User = mongoose.model("User", userSchema);
