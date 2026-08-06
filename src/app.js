080f42070e4e4b052e7831f264795002e18b465f
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-08-06 23:41:05 +0530

    feat: add cookie-parser middleware for handling cookies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(
 );
 
 app.use(express.static("public"));
+app.use(cookieParser());
 
 
 
