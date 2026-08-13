8f8b4e48973a4612547bb09e45e705fa0b8ffb73
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-08-14 00:51:36 +0530

    feat: add multer middleware for file uploads and integrate it into task creation

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -8,32 +8,88 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
-
 const getTasks = asyncHandler(async (req, res) => {
-    //
-})
+    const { projectId } = req.params;
+
+    const project = await Project.findById(projectId)
+
+    if(!project) {
+        throw new ApiError(404, "Project not found")
+    }
+
+    const tasks = await Task.find({
+        project: new mongoose.Types.ObjectId(projectId)
+    }).populate("assignedTo", "avatar username fullName")
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tasks,
+                "Tasks fetched successfully"
+            )
+        )
+});
 const createTasks = asyncHandler(async (req, res) => {
-    //
-})
+    const { title, description, assignedTo, status } = req.body;
+    const { projectId } = req.params;
+
+    const project = await Project.findById(projectId)
+
+    if(!project) {
+        throw new ApiError(404, "Project not found")
+    }
+
+    const files = req.files || []
+
+    const attachments = files.map((file) => {
+        return {
+            url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+            mimetype: file.mimetype,
+            size: file.size
+        }
+    })
+
+    const task = await Task.create({
+        title,
+        description,
+        project: new mongoose.Types.ObjectId(projectId),
+        attachments,
+        createdBy: req.user._id,
+        assignedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo) : undefined,
+        status,
+        assignedBy: new mongoose.Types.ObjectId(req.user._id)
+    })
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                201, 
+                task,
+                "Task created successfully"
+            )
+        )
+});
 const getTaskById = asyncHandler(async (req, res) => {
     //
-})
+});
 const updateTask = asyncHandler(async (req, res) => {
     //
-})
+});
 const deleteTasks = asyncHandler(async (req, res) => {
     //
-})
+});
 const createSubTask = asyncHandler(async (req, res) => {
     //
-})
+});
 const updateSubTask = asyncHandler(async (req, res) => {
     //
-})
+});
 const deleteSubTask = asyncHandler(async (req, res) => {
     //
-})
-
+});
 
 export {
     createTasks,
@@ -43,5 +99,5 @@ export {
     deleteTasks,
     createSubTask,
     updateSubTask,
-    deleteSubTask
-}
+    deleteSubTask,
+};
